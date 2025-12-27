f5e1f81a241aafef39d782a16c73de9dc722cc20
soumya-sunkad <soumyasunkad06@gmail.com>
2025-12-27 13:22:09 +0530

    Correct expected output format for student details
    Fix expected output formatting in text_student_details function.

--- text_student.py
+++ text_student.py
@@ -1,9 +1,9 @@
 from student import student_details
 def text_student_details():
     expected_output=(
-        "Student USN:01fe24bca274\n"
-        "Student name:soumya\n"
-        "Division:E\n"
+        "Student USN:01fe24bca274"
+        "Student name:soumya"
+        "Division:E"
         "Age:19"
     )
     assert student_details("01fe24bca274","soumya","E",19)== expected_output
